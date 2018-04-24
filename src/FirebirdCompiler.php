f78cdf69c297ef8631d8e74090cf29bbb1307cea
arilia <andrea.mazzocchi@gmail.com>
2018-04-24 12:58:45 +0200

    deprecations

--- src/FirebirdCompiler.php
+++ src/FirebirdCompiler.php
@@ -51,7 +51,7 @@ class FirebirdCompiler extends QueryCompiler
      */
     protected function _buildSelectPart($parts, $query, $generator)
     {
-        $driver = $query->connection()->driver();
+        $driver = $query->getConnection()->getDriver();
         $select = 'SELECT %s%s%s';
         if ($this->_orderedUnion && $query->clause('union')) {
             $select = '(SELECT %s%s%s';
